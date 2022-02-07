46b4248f402c4409edb861e8a756be912926dd59
Erik Salvesen <eriksalv@stud.ntnu.no>
2022-02-07 17:46:07 +0100

    ci: satt opp frontend test-stage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    async function backendStatusCheck() {
-      const response = await axios.get("http://0.0.0.0:4000");
-      return response.data.status;
-    }
-    backendStatusCheck().then((status) => console.log("response ", status));
-  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
