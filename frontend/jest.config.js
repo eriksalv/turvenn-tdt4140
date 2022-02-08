8ec17db5206fe2fad823c9f5952e2b4824448872
eriksalv <eriksalv@stud.ntnu.no>
2022-02-08 17:16:17 +0100

    style(#3): eslint og prettier på frontend

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   clearMocks: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   collectCoverage: false,
   collectCoverageFrom: [
-    "**/src/**/*.{js,jsx,ts}",
-    "!**/types.js",
-    "!**/src/main.js",
-    "!**/node_modules/**",
-    "!**/*.config.js",
-    "!**/coverage/**",
-    "!**/index.js",
+    '**/src/**/*.{js,jsx,ts}',
+    '!**/types.js',
+    '!**/src/main.js',
+    '!**/node_modules/**',
+    '!**/*.config.js',
+    '!**/coverage/**',
+    '!**/index.js'
   ],
-  moduleFileExtensions: ["js", "json", "ts", "node", "jsx"],
+  moduleFileExtensions: ['js', 'json', 'ts', 'node', 'jsx'],
   transform: {
-    "^.+\\.js$": "babel-jest",
-    ".*\\.jsx$": "babel-jest",
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.jsx$': 'babel-jest'
   },
   moduleNameMapper: {
-    "^.+\\.(css|less)$": "<rootDir>/config/CSSStub.js",
-  },
+    '^.+\\.(css|less)$': '<rootDir>/config/CSSStub.js'
+  }
 };
