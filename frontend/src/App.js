a843c72fb5498288741476a59180d7be8e860d0e
eriksalv <eriksalv@stud.ntnu.no>
2022-02-08 14:45:38 +0100

    feat(#4): satt opp react-router og laget en enkel login-fane
    Co-Authored-By: Ola <@olahja>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-
-import "./App.css";
+import Login from './pages/Login';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
