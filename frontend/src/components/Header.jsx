c5379b97f495503d95a29c5fde9dab9c91ed514f
eriksalv <eriksalv@stud.ntnu.no>
2022-02-10 12:04:04 +0100

    feat(#4): lagt til material ui

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+function Header() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default Header;
