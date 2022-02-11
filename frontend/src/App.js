e974a15abc83af68a16f1b233c604c97c3740a0a
Erik Salvesen <eriksalv@stud.ntnu.no>
2022-02-11 10:14:38 +0100

    feat(#4): litt mer redux boilderplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </Router>
   );
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,16 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-  email: "test@test.com",
+  user: null,
+  isLoading: false,
+  isError: false,
+  message: "test",
 };
+
+export const register = createAsyncThunk(
+  "/auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    reset: (state) => initialState,
-  },
+  reducers: {},
+  extraReducers: (builder) => {},
 });
 
 export const { reset } = authSlice.actions;
+
 export default authSlice.reducer;
