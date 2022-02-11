ec16a0d5c42c4618e2c668d473e7e185b7773b4d
eriksalv <eriksalv@stud.ntnu.no>
2022-02-11 15:25:31 +0100

    oppdatert server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const config = require('./config/config.json').development;
 const Sequelize = require('sequelize');
+const config = require('./config/config.json').development;
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 exports.sequelize = sequelize;
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+console.log('Det funker nå?');
+
 const app = express();
 const PORT = 4000;
+
+app.disable('etag');
 
 app.use(bodyparser.json({ limit: '50mb' }));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use('/user', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.header('Origin'));
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader('Access-Control-Allow-Origin', req.header('Origin'));
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+app.use('/api/users', require('./routes/user'));
 
 app.get('/', (req, res) => {
   res.status(200).send({ status: 'ok' });
