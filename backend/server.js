586a97d907ccaa7efb4ee57ed685cc1300c44303
Andreas Netteland <andreas@disputas.no>
2022-02-11 14:41:58 +0100

    fix hot reload and commands

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const config = require('./config/config.json').development;
+const Sequelize = require('sequelize');
 
-export const sequelize = new Sequelize(config.database, config.username, config.password, config);
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+exports.sequelize = sequelize;
+
 const app = express();
 const PORT = 4000;
 
