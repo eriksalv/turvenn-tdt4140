41a7f0ec31522ee79cca8fd495c397d9e6471d15
trygvemm <trygvemm@stud.ntnu.no>
2022-02-15 11:45:53 +0100

    feat(#4): Login page MUI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom'
+
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -22,26 +27,40 @@ function Login() {
 
   return (
     <div>
-      <h1 className="">Logg inn</h1>
       <form onSubmit={onSubmit}>
-        <label htmlFor="email">Epost</label>
-        <input
-          type="text"
-          value={email}
-          id="email"
-          name="email"
-          onChange={onChange}
-        />
-        <label htmlFor="password">Passord</label>
-        <input
-          type="password"
-          value={password}
+      <Grid
+      container spacing={2} columnSpacing={{ xs: 0, sm: 0, md: 0 }}
+      direction="column"
+      alignItems="center"
+      >
+<Grid item xs={12}><h1 className="">Logg inn</h1></Grid>
+
+<Grid item xs={12}>
+        <TextField 
+        required
+        id="email"  
+        
+        label="Email"
+         variant="outlined"  
+         onChange={onChange} />
+         </Grid>
+        <Grid item xs={12}>
+        <TextField
+        
+        required
           id="password"
-          name="password"
-          onChange={onChange}
+          label="Passord"
+          type="password"
         />
-        <button className="btn btn-success">Logg inn</button>
-      </form>
+        </Grid>
+        <Grid item xs={5}>
+        <Button variant="contained" id="onSubmit" color="success">Logg inn</Button>
+        </Grid>
+        <Grid item xs={6}>
+        <Link to="/register">Registrer bruker</Link>
+        </Grid>
+        </Grid>
+    </form>
     </div>
   );
 }
