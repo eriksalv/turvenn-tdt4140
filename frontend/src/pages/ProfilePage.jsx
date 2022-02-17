e1c5c5e292322fa9ab425595b50279aec6d01f12
Sondre Rogde <sondre.rogde@gmail.com>
2022-02-17 11:51:39 +0100

    feat(#2): la til profilside
    Co-Authored-By:@eriksalv
    Co-Authored-By:@alvaha
    Co-Authored-By:andreone

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,49 +1,94 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { register } from "../features/auth/authSlice";
-import LoadingButton from '@mui/material/Button';
-import SaveIcon from '@mui/icons-material/Save';
-import  TextField from "@mui/material/TextField";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import LoadingButton from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TripCard from "../components/TripCard";
+import { Divider, Chip } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 import "./ProfilePage.css";
 
-function ProfilePage(){
-  return <div>
-    <div id = "information">
-      <div id = "profilePic">
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%",
+});
 
-      </div>
-      <div id="about">
-        <TextField
-              className="aboutText"
-              id="outlined-multiline-static"
-              label="Biografi"
-              multiline
-              rows={4}
-              defaultValue="Skriv noe om deg selv"
+function ProfilePage() {
+  const mockData = [
+    {
+      difficulty: 1,
+      duration: "2 days",
+      date: "25.02.2022",
+      id: "dkawdopwa",
+    },
+    {
+      difficulty: 3,
+      duration: "4 days",
+      date: "15.03.2022",
+      id: "jdpowadjwpa",
+    },
+    {
+      difficulty: 5,
+      duration: "3 days",
+      date: "01.04.2022",
+      id: "jdiwajdipaw",
+    },
+  ];
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div id="information">
+        <div id="profilePic">
+          <Img
+            alt="En kul tur med gode venner"
+            src={"../assets/Turvenn-2.png"}
+          />
+        </div>
+        <form id="about">
+          <TextField
+            className="aboutText"
+            id="outlined-multiline-static"
+            label="Biografi"
+            multiline
+            rows={4}
+            defaultValue="Skriv noe om deg selv"
           />
           <LoadingButton
-              id ="aboutSave"
-              loading
-              loadingPosition="start"
-              startIcon={<SaveIcon />}
-              variant="outlined"
-          > Lagre 
+            id="aboutSave"
+            loading
+            loadingPosition="start"
+            startIcon={<SaveIcon />}
+            variant="outlined"
+          >
+            {" "}
+            Lagre
           </LoadingButton>
-          <Box sx = {{minWidth:120}} className ="experienceBox">
+          <Box sx={{ minWidth: 120 }} className="experienceBox">
             <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Erfaringsnivå</InputLabel>
+              <InputLabel id="demo-simple-select-label">
+                Erfaringsnivå
+              </InputLabel>
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 value={20}
                 label="Erfaringsnivå"
-                
               >
                 <MenuItem value={10}>Nybegynner</MenuItem>
                 <MenuItem value={20}>Erfaren</MenuItem>
@@ -51,19 +96,35 @@ function ProfilePage(){
               </Select>
             </FormControl>
           </Box>
+        </form>
       </div>
-    </div>
 
-    <div id = "history">
+      <Divider sx={{ width: "100%" }}>
+        <Chip label="Turhistorikk" />
+      </Divider>
+      <Box
+        id="tripHistoryContainer"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {mockData.map((item) => (
+          <TripCard
+            difficulty={item.difficulty}
+            duration={item.duration}
+            date={item.date}
+            key={item.id}
+          />
+        ))}
+      </Box>
 
-
-    </div>
-    
-
-
-
-
-  </div>
+      <div id="history"></div>
+    </Box>
+  );
 }
 
 export default ProfilePage;
