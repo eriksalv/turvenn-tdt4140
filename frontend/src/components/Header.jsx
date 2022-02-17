719c9ef04a174c6256aca1d917340c0d121cc633
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2022-02-17 11:39:50 +0100

    feat(#15): fikset header tekst

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -42,7 +42,7 @@ function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            {user ? `Hei ${user.firstName}!` : 'Turvenn'}
           </Typography>
           {user ? (
             <Button color="inherit" onClick={onLogout}>
