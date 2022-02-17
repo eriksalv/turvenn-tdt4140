969d10f94493671e4d30879ab750a04081ef2221
andressc <andressc@stud.ntnu.no>
2022-02-17 15:51:13 +0100

    fix(#9) Fiksen inputfelt varighet og beskrivelse + plassering av button
    Co-Authored-By: @eriksalv
    Co-Authored-By: @andreone

--- frontend/src/pages/CreateTrip.jsx
+++ frontend/src/pages/CreateTrip.jsx
@@ -3,7 +3,6 @@ import {
   Grid,
   Button,
   Paper,
-  Avatar,
   TextField,
   FormControl,
   InputLabel,
@@ -22,7 +21,7 @@ function CreateTrip() {
   });
 
   const { goal, start, date, difficulty, duration, description } = formData;
-  const paperStyle = { padding: 20, height: '90vh', width: 350, margin: '20px auto' };
+  const paperStyle = { padding: 20, width: 900, margin: '20px auto' };
 
   const onChange = (e) => {
     console.log(e.target.value);
@@ -42,88 +41,102 @@ function CreateTrip() {
     setFormData((prevState) => ({ ...prevState, difficulty: e.target.value }));
   };
 
+  const onChangeDescription = (e) => {
+    setFormData((prevState) => ({ ...prevState, description: e.target.value }));
+  };
+
   return (
     <main>
       <Grid>
         <Paper elevation={10} style={paperStyle}>
-          <Grid align="center">
-            <Avatar>T</Avatar>
+          <Grid align="left">
             <h2>Opprett turarrangement</h2>
           </Grid>
-
-          <Grid>
-            <form onSubmit={onSubmit}>
-              <TextField
-                id="goal"
-                label="Turmål"
-                placeholder="Hvor går turen?"
-                required
-                fullWidth
-                value={goal}
-                margin="normal"
-                onChange={onChange}
-              />
-              <TextField
-                id="start"
-                label="Startpunkt"
-                placeholder="Hvor starter turen?"
-                required
-                fullWidth
-                value={start}
-                margin="normal"
-                onChange={onChange}
-              />
-              <TextField
-                id="date"
-                label="Dato og tid"
-                type="datetime-local"
-                required
-                fullWidth
-                value={date}
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true
-                }}
-                onChange={onChange}
-              />
-              <FormControl id="difficulty" fullWidth>
-                <InputLabel>Vanskelighetsgrad</InputLabel>
-                <Select
-                  labelId="difficulty"
-                  id="difficulty"
-                  value={difficulty}
-                  label="Vanskelighetsgrad"
-                  onChange={onChangeDifficulty}
-                >
-                  <MenuItem value="enkel">Enkel</MenuItem>
-                  <MenuItem value="middels">Middels</MenuItem>
-                  <MenuItem value="krevende">Krevende</MenuItem>
-                  <MenuItem value="ekstraKrevende">Ekstra krevende</MenuItem>
-                </Select>
-              </FormControl>
-              <TextField
-                id="duration"
-                label="Varighet"
-                placeholder="Hvor lenge varer turen?"
-                fullWidth
-                value={duration}
-                margin="normal"
-                onChange={onChange}
-              />
-              <textarea
-                id="descrition"
-                label="Beskrivelse"
-                placeholder="Beskriv turarrangementet her"
-                rows={6}
-                cols={41}
-                value={description}
-                margin="normal"
-                onChange={onChange}
-              />
-              <Button className="btn btn-success" type="submit" variant="contained" fullWidth>
-                Opprett turarrangement
-              </Button>
-            </form>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              Bilde
+            </Grid>
+            <Grid item xs={8}>
+              <form onSubmit={onSubmit}>
+                <TextField
+                  id="goal"
+                  label="Turmål"
+                  placeholder="Hvor går turen?"
+                  required
+                  fullWidth
+                  value={goal}
+                  margin="normal"
+                  onChange={onChange}
+                />
+                <TextField
+                  id="start"
+                  label="Startpunkt"
+                  placeholder="Hvor starter turen?"
+                  required
+                  fullWidth
+                  value={start}
+                  margin="normal"
+                  onChange={onChange}
+                />
+                <TextField
+                  id="date"
+                  label="Dato og tid"
+                  type="datetime-local"
+                  required
+                  fullWidth
+                  value={date}
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={onChange}
+                />
+                <FormControl id="difficulty" fullWidth>
+                  <InputLabel>Vanskelighetsgrad</InputLabel>
+                  <Select
+                    labelId="difficulty"
+                    id="difficulty"
+                    value={difficulty}
+                    label="Vanskelighetsgrad"
+                    onChange={onChangeDifficulty}
+                  >
+                    <MenuItem value="enkel">Enkel</MenuItem>
+                    <MenuItem value="middels">Middels</MenuItem>
+                    <MenuItem value="krevende">Krevende</MenuItem>
+                    <MenuItem value="ekstraKrevende">Ekstra krevende</MenuItem>
+                  </Select>
+                </FormControl>
+                <TextField
+                  id="duration"
+                  label="Varighet"
+                  placeholder="Hvor lenge varer turen?"
+                  fullWidth
+                  value={duration}
+                  margin="normal"
+                  onChange={onChange}
+                />
+                <textarea
+                  id="descrition"
+                  label="Beskrivelse"
+                  placeholder="Beskriv turarrangementet her"
+                  rows={6}
+                  cols={72}
+                  value={description}
+                  margin="normal"
+                  onChange={onChangeDescription}
+                />
+              </form>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            alignItems="flex-end"
+            justifyContent="flex-end"
+            sx={{ mt: '4rem', mb: '0.5rem' }}
+          >
+            <Button className="btn btn-success" type="submit" variant="contained">
+              Opprett turarrangement
+            </Button>
           </Grid>
         </Paper>
       </Grid>
