7ef1a942f5f68135a0b5aa9bffe6aada8780d964
eriksalv <eriksalv@stud.ntnu.no>
2022-02-18 16:44:09 +0100

    test(#21): testet getUserTrips

--- backend/tests/unit/tripController.test.js
+++ backend/tests/unit/tripController.test.js
@@ -1,5 +1,5 @@
-const { Trip } = require('../../models');
-const { createTrip } = require('../../controllers/tripController');
+const { User, Trip } = require('../../models');
+const { createTrip, getUserTrips } = require('../../controllers/tripController');
 
 describe('Trip Controller - Create Trip', () => {
   let req;
@@ -49,5 +49,53 @@ describe('Trip Controller - Create Trip', () => {
 
     expect(res.status).toHaveBeenCalledWith(400);
     expect(next).toHaveBeenCalledWith(new Error('Invalid trip data'));
+  });
+});
+
+describe('Trip Controller - getUserTrips', () => {
+  let req;
+  let res;
+  let next;
+  beforeEach(() => {
+    req = {
+      params: {
+        userId: 1
+      }
+    };
+    res = {
+      status: jest.fn().mockImplementation(() => res),
+      json: jest.fn()
+    };
+    next = jest.fn();
+  });
+
+  it('should return status 200 with users trips if successful', async () => {
+    const trips = [
+      {
+        name: 'trip 1'
+      },
+      {
+        name: 'trip 2'
+      }
+    ];
+    const user = {
+      getTrips: jest.fn().mockImplementation(() => trips)
+    };
+    User.findByPk = jest.fn().mockImplementation(() => user);
+
+    await getUserTrips(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(trips);
+    expect(User.findByPk).toHaveBeenCalledWith(req.params.userId);
+  });
+
+  it('should set status to 404 if no user is found', async () => {
+    User.findByPk = jest.fn().mockImplementation(() => null);
+
+    await getUserTrips(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(next).toHaveBeenCalledWith(new Error('User not found'));
   });
 });
