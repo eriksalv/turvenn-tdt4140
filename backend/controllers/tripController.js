35830fe957bf1c9248e3dac6ffb8124f72b114fb
Erik Salvesen <eriksalv@stud.ntnu.no>
2022-02-24 19:30:20 +0100

    feat(#33): laget API-ruter for signOff og participators

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -113,4 +113,55 @@ const signUp = async (req, res, next) => {
   }
 };
 
-module.exports = { getTrips, createTrip, getUserTrips, getTrip, signUp };
+const signOff = async (req, res, next) => {
+  const { tripId } = req.params;
+  const userId = req.user.id;
+
+  const user = await User.findByPk(userId);
+  const trip = await Trip.findByPk(tripId);
+
+  if (!trip) {
+    res.status(404);
+    return next(new Error('Trip not found'));
+  }
+
+  try {
+    await user.removeParticipatedTrip(trip);
+    return res.status(200).json({ message: 'Successfully signed off' });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    next(new Error('Something went wrong'));
+  }
+};
+
+const getParticipators = async (req, res, next) => {
+  const { tripId } = req.params;
+  const trip = await Trip.findByPk(tripId, {
+    attributes: ['id', 'name', 'start', 'goal', 'date', 'difficulty', 'duration', 'description'],
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'firstName', 'lastName', 'email']
+      },
+      {
+        model: User,
+        as: 'participators',
+        attributes: ['id', 'firstName', 'lastName', 'email'],
+        through: {
+          attributes: []
+        }
+      }
+    ]
+  });
+
+  if (!trip) {
+    res.status(404);
+    return next(new Error('Trip not found'));
+  }
+
+  return res.status(200).json(trip);
+};
+
+module.exports = { getTrips, createTrip, getUserTrips, getTrip, signUp, signOff, getParticipators };
--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -2,7 +2,14 @@ const express = require('express');
 const { body } = require('express-validator');
 const { protect } = require('../middleware/authHandler');
 
-const { getTrips, createTrip, getTrip, signUp } = require('../controllers/tripController');
+const {
+  getTrips,
+  createTrip,
+  getTrip,
+  signUp,
+  signOff,
+  getParticipators
+} = require('../controllers/tripController');
 
 const router = express.Router();
 
@@ -15,6 +22,13 @@ router.get('/:tripId', getTrip);
 // POST /api/trips
 router.post('/', protect, createTrip);
 
+// POST /api/trips/:tripId/signup
 router.post('/:tripId/signup', protect, signUp);
+
+// DELETE /api/trips/:tripId/signup
+router.delete('/:tripId/signup', protect, signOff);
+
+// GET /api/trips/:tripId/participators
+router.get('/:tripId/participators', getParticipators);
 
 module.exports = router;
