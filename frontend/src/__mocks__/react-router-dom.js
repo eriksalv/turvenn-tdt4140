de001d69bdf979e9c937f9a4f3bfa80bf44158ef
Erik Salvesen <eriksalv@stud.ntnu.no>
2022-03-16 00:12:16 +0100

    test(#42): litt mer setup for testing i frontend

--- frontend/src/__mocks__/react-router-dom.js
+++ frontend/src/__mocks__/react-router-dom.js
@@ -1,6 +1,7 @@
 const useNavigate = jest.fn();
 const useLocation = jest.fn();
+const useParams = jest.fn();
 function Link() {
   return <div>Link</div>;
 }
-module.exports = { useNavigate, useLocation, Link };
+module.exports = { useNavigate, useLocation, useParams, Link };
---
+++ frontend/src/tests/components/User.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'regenerator-runtime/runtime';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import * as reactRouter from 'react-router-dom';
+import * as redux from 'react-redux';
+import User from '../../pages/User';
+
+let selectorSpy;
+let paramsSpy;
+
+beforeAll(() => {
+  selectorSpy = jest.spyOn(redux, 'useSelector');
+  selectorSpy.mockReturnValue({
+    user: {
+      firstName: 'Test',
+      lastName: 'Test',
+      email: 'email@email.com',
+      password: 'password',
+      role: 'user'
+    },
+    userTrips: [
+      {
+        id: 1,
+        name: 'Test'
+      }
+    ]
+  });
+  paramsSpy = jest.spyOn(reactRouter, 'useParams');
+  paramsSpy.mockReturnValue({
+    id: 1
+  });
+});
+
+it('should render component', () => {
+  render(<User />);
+
+  const name = screen.getByRole('heading', {
+    name: /test test/i
+  });
+
+  expect(name).not.toBe(null);
+  expect(name).toBeInTheDocument();
+});
