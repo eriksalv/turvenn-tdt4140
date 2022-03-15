c1e6cb43b156de61e6b575a7968ebaff7fdf1f54
Erik Salvesen <eriksalv@stud.ntnu.no>
2022-03-15 20:25:07 +0100

    feat(#42): admin kan slette tur i backend

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -185,7 +185,7 @@ const signOff = async (req, res, next) => {
 };
 
 const deleteTrip = async (req, res, next) => {
-  const userId = req.user.id;
+  const { id, role } = req.user;
   const { tripId } = req.params;
 
   const trip = await Trip.findByPk(tripId);
@@ -197,7 +197,8 @@ const deleteTrip = async (req, res, next) => {
 
   try {
     // TODO(Ola): gjøre så "groups" kan slette, ikke basert på brukerid for at admin kan også slette
-    if (trip.userId !== userId) return res.status(403).json({ message: 'Unauthorized' });
+    if (role !== 'admin' && trip.userId !== id)
+      return res.status(403).json({ message: 'Unauthorized' });
 
     await trip.destroy();
 
