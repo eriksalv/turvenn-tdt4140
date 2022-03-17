cf51c9767c6de53362fa7d91e49aa7564841a9bd
andressc <andressc@stud.ntnu.no>
2022-03-17 16:38:21 +0100

    feat(#51) lagd felt for å skrive tekstlogger og laste opp bilder.
    Co-Authored-By: @eriksalv @alvaha

---
+++ frontend/src/components/LogCard.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import { useNavigate } from 'react-router-dom';
+
+const Img = styled('imgurl')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%'
+});
+
+export default function LogCard({ id, text, imgurl = '../assets/defaultHike.jpeg' }) {
+  const navigate = useNavigate();
+
+  const onClick = () => {};
+
+  return (
+    <Paper
+      onClick={onClick}
+      sx={{
+        width: '400px',
+        maxWidth: '400px',
+        p: 2,
+        margin: 2,
+        marginBottom: '2rem',
+        flexGrow: 1,
+        ':hover': {
+          boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.75)'
+        },
+        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#1A2027' : '#fff')
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <ButtonBase sx={{ width: 160, height: 120 }}>
+            <Img alt="Nice bilde fra turen" src={imgurl} />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={6} sm container>
+          <Typography variant="body2" gutterBottom>
+            {text}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
--- frontend/src/pages/ViewTrip.jsx
+++ frontend/src/pages/ViewTrip.jsx
@@ -1,7 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Grid, Paper, styled, Divider, Chip, Box, Typography, Button } from '@mui/material';
+import {
+  Grid,
+  Paper,
+  styled,
+  Divider,
+  Chip,
+  Box,
+  Typography,
+  Button,
+  Avatar,
+  TextField
+} from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -14,20 +25,46 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import moment from 'moment';
 import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import PublishIcon from '@mui/icons-material/Publish';
 import useSignedUpStatus from '../hooks/useSignedUpStatus';
-
 import { getTrip, reset, signUp, signOff, deleteTrip } from '../features/trips/tripSlice';
 
 import ProfileCard from '../components/ProfileCard';
+import LogCard from '../components/LogCard';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%'
+});
 
 function ViewTrip() {
-  const Img = styled('img')({
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%'
-  });
+  const mockData = [
+    {
+      text: 'Nydelig tur!',
+      imgurl: '../assets/defaultHike.jpeg',
+      id: 'dkawdopwa'
+    },
+    {
+      text: 'Ut på tur aldri sur!',
+      imgurl: '../assets/defaultHike.jpeg',
+      id: 'bcjhscfre'
+    },
+    {
+      text: 'Utrolig vær og utrolige turkammerater!',
+      imgurl: '../assets/defaultHike.jpeg',
+      id: 'fewjbhjrwfr'
+    }
+  ];
   const paperStyle = { padding: 20, maxWidth: 900, margin: '20px auto' };
+  const [formData, setFormData] = useState({
+    text: '',
+    imgurl: ''
+  });
+  const { text, imgurl } = formData;
 
   const [signedUp, setSignedUp] = useSignedUpStatus()[0];
   const [checkingStatus] = useSignedUpStatus()[1];
@@ -83,6 +120,17 @@ function ViewTrip() {
 
   const onDelete = () => {
     dispatch(deleteTrip(trip.id));
+  };
+
+  const onChangeLog = (e) => {
+    setFormData((prevState) => ({ ...prevState, description: e.target.value }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const logData = { text, imgurl };
+
+    // dispatch(createLog(logData));
   };
 
   if (isLoading || checkingStatus || !trip) {
@@ -185,6 +233,95 @@ function ViewTrip() {
             justifyContent="flex-start"
             sx={{ mt: '4rem', mb: '0.5rem' }}
           >
+            <Divider sx={{ width: '100%' }}>
+              <Chip label="Innlegg" />
+            </Divider>
+
+            <Paper
+              sx={{ mt: '1rem', mb: '1rem' }}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                padding: '10px'
+              }}
+            >
+              <Grid
+                container
+                spacing={2}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '100%'
+                }}
+              >
+                <Grid item xs={1}>
+                  <Avatar size="large">
+                    <AccountCircleIcon />
+                  </Avatar>
+                </Grid>
+                <Grid item xs={10}>
+                  <TextField
+                    id="text"
+                    label="Innlegg"
+                    placeholder="Hvordan gikk turen?"
+                    multiline
+                    fullWidth
+                    margin="normal"
+                    size="small"
+                    value={text}
+                    onChange={onChangeLog}
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <Button
+                    id="imgurl"
+                    variant="contained"
+                    component="label"
+                    size="large"
+                    margin="normal"
+                  >
+                    <AddPhotoAlternateIcon />
+                    <input type="file" hidden value={imgurl} />
+                  </Button>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                alignItems="flex-end"
+                justifyContent="flex-end"
+                sx={{ mt: '0.5rem', mb: '0.2rem' }}
+              >
+                <Button
+                  onClick={onSubmit}
+                  className="btn btn-success"
+                  type="submit"
+                  variant="contained"
+                  endIcon={<PublishIcon />}
+                >
+                  Publiser innlegg
+                </Button>
+              </Grid>
+            </Paper>
+            <Box
+              id="postCardContainer"
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContant: 'center',
+                alignItems: 'center'
+              }}
+            >
+              {mockData.map((item) => (
+                <LogCard key={item.id} id={item.id} text={item.text} imgurl={item.imgurl} />
+              ))}
+            </Box>
+
             <Divider sx={{ width: '100%' }}>
               <Chip label="Turgåere" />
             </Divider>
